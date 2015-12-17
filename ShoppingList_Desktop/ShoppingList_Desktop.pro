f55acd13fc596b1a61e246bffde42cf7c2cd487b
jklmnn <jklmnn@web.de>
2015-12-17 12:23:43 +0100

    Added project files with simple layout.

---
+++ ShoppingList_Desktop/ShoppingList_Desktop.pro
@@ -0,0 +1,20 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2015-12-17T11:33:21
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = ShoppingList_Desktop
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h
+
+FORMS    += mainwindow.ui
---
+++ ShoppingList_Desktop/mainwindow.cpp
@@ -0,0 +1,23 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    this->setAddRow();
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::setAddRow(){
+    int rowid = ui->itemTable->rowCount();
+    ui->itemTable->setRowCount(rowid + 1);
+    ui->itemTable->setCellWidget(rowid, 0, itemedit);
+    ui->itemTable->setCellWidget(rowid, 1, countedit);
+    ui->itemTable->setCellWidget(rowid, 3, addbutton);
+}
