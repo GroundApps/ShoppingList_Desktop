a41c6d52733c4e44ec47fbc85d3208020746b737
jklmnn <jklmnn@web.de>
2015-12-17 12:24:43 +0100

    Forgot to save before commit.

--- ShoppingList_Desktop/mainwindow.cpp
+++ ShoppingList_Desktop/mainwindow.cpp
@@ -6,7 +6,8 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    this->setAddRow();
+    addbutton->setText("Add");
+    setAddRow();
 }
 
 MainWindow::~MainWindow()
